2d9ccb24c793546a77c864c180100ef7a5ad7f40
ErikPresnov <erik.presnov160@gmail.com>
2023-10-23 10:48:37 +0300

    Lisa

--- src/main/java/com/example/graphgrader/GraphGrader.java
+++ src/main/java/com/example/graphgrader/GraphGrader.java
@@ -11,7 +11,7 @@ public class GraphGrader extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GraphGrader.class.getResource("Baas.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
         stage.setTitle("Graafi algoritmite läbimängija");
         stage.setScene(scene);
         stage.setResizable(false);
